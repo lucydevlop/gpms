96da969de9c5c0268a627c697657c45431100736
Lucy-dev <lucy.kim228@gmail.com>
2020-12-10 14:45:23 +0900

    할인권 조회 API

--- src/main/kotlin/io/glnt/gpms/handler/discount/controller/DiscountController.kt
+++ src/main/kotlin/io/glnt/gpms/handler/discount/controller/DiscountController.kt
@@ -1,14 +1,25 @@
 package io.glnt.gpms.io.glnt.gpms.handler.discount.controller
 
 import io.glnt.gpms.common.configs.ApiConfig.API_VERSION
+import io.glnt.gpms.exception.CustomException
+import io.glnt.gpms.handler.discount.service.DiscountService
+import mu.KLogging
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(path=["/$API_VERSION/discount"])
+@RequestMapping(path=["/$API_VERSION/ticket"])
 @CrossOrigin(origins = arrayOf("*"), allowedHeaders = arrayOf("*"))
 class DiscountController {
 
+    @Autowired
+    private lateinit var discountService: DiscountService
 
+//    @RequestMapping(value = ["/list"], method = [RequestMethod.POST])
+//    @Throws(CustomException::class)
+
+    companion object : KLogging()
 }
---
+++ src/main/kotlin/io/glnt/gpms/handler/discount/model/reqSearchDiscount.kt
@@ -0,0 +1,5 @@
+package io.glnt.gpms.io.glnt.gpms.handler.discount.model
+
+data class reqSearchDiscount(
+    var corpId: String? = null
+)
