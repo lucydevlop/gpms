bfabac8b3d36a0f1f3eef03b7a7c865b6c800ce7
Lucy-dev <lucy.kim228@gmail.com>
2021-01-29 17:47:11 +0900

    입주사 조회 API

--- src/main/kotlin/io/glnt/gpms/handler/corp/model/reqSearchCorp.kt
+++ src/main/kotlin/io/glnt/gpms/handler/corp/model/reqSearchCorp.kt
@@ -1,5 +1,8 @@
 package io.glnt.gpms.handler.corp.model
 
 data class reqSearchCorp(
-    var corpId: String? = null
+    var corpId: String? = null,
+    var searchLabel: String? = null,
+    var searchText: String? = null,
+    var useStatus: String? = "ALL"
 )
