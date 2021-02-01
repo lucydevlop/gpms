2e4c9e2ea8890e298064640dbeee10745f2d8005
Lucy-dev <lucy.kim228@gmail.com>
2021-02-01 17:47:06 +0900

    입주사 조회 사용여부 enum type으로 수정

--- src/main/kotlin/io/glnt/gpms/handler/corp/model/reqSearchCorp.kt
+++ src/main/kotlin/io/glnt/gpms/handler/corp/model/reqSearchCorp.kt
@@ -1,8 +1,12 @@
 package io.glnt.gpms.handler.corp.model
 
+import io.glnt.gpms.model.enums.DelYn
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+
 data class reqSearchCorp(
     var corpId: String? = null,
     var searchLabel: String? = null,
     var searchText: String? = null,
-    var useStatus: String? = "ALL"
+    @Enumerated(EnumType.STRING) var useStatus: DelYn? = null
 )
